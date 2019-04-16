95a66b0a7e2dc5159745509f07c4f985ccf38f5b
jedrojas <jprojas15@gmail.com>
2019-04-16 16:54:26 -0400

    first commit

---
+++ contestmeister.py
@@ -0,0 +1,212 @@
+# Allison Kahn
+# CNT4007C
+# Project 1
+# Code based on https://stackoverflow.com/questions/21233340/sending-string-via-socket-python
+#!/usr/bin/env python3
+import socket
+import json
+import sys
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    hostname = sys.argv[1]
+    port = sys.argv[2]
+except:
+    print("Missing arguments. Closing.")
+    exit()
+
+try:
+    s.connect((hostname, int(port)))
+except EnvironmentError as exc:
+    print("Cannot create connection. Closing.")
+    s.close()
+    exit()
+qtype = ''
+questionDict = {}
+
+
+while qtype != '1':
+
+    qtype = input("> ")
+
+    qtypefirst = qtype[:1]
+    qtyperest = qtype[1:]
+
+    if qtypefirst == 'p':
+
+        sendData = json.dumps(qtype)
+        s.send(sendData.encode())
+        rcvdData = s.recv(1024).decode()
+        response = json.loads(rcvdData)
+
+        if response[0:5] == "Error":
+            print(response)
+        else:
+            tags = ""
+            tags = input("")
+            # tags = tags.split(",")
+            qtemp = ""
+            question = ""
+            while(qtemp != "."):
+                qtemp = input("")
+                question = question + qtemp
+            question = question[:-1]
+            listOfQ = []
+            prevAnswer = "Temp"
+            currAnswer = ""
+            currAnswerTemp = " "
+            counter = 0
+            while counter < 1:
+                while counter < 1:
+                    currAnswerTemp = input("")
+                    currAnswer = currAnswer + currAnswerTemp
+                    if currAnswer == ".":
+                        counter = counter + 1
+                    if(currAnswerTemp == "."):
+                        break
+                currAnswer = currAnswer[:-1]
+                if(currAnswer != ""):
+                    if counter == 0:
+                        counter = 0
+                    else:
+                        counter = counter - 1
+                    listOfQ.append(currAnswer)
+                    currAnswer = ""
+                if len(listOfQ) < 2 and counter == 1:
+                    print("Must have at least 2 choices. Please enter another option.")
+                    counter = counter - 1
+                currAnswerTemp = ""
+            correctAnswer = input("")
+
+            ###
+
+            sendData = json.dumps("GetNextQNum")
+            s.send(sendData.encode())
+            rcvdData = s.recv(1024).decode()
+            questionNum = json.loads(rcvdData)
+            ###
+
+            tempQuestion = {}
+
+            wholeQuestion = []
+            wholeQuestion.append(tags)
+            wholeQuestion.append(question)
+            wholeQuestion.append(listOfQ)
+            wholeQuestion.append(correctAnswer)
+            tempQuestion[questionNum] = wholeQuestion
+            # print(questionNum)
+            print("Question " + questionNum + " added.")
+
+            sendData = (tempQuestion)
+            sendData = json.dumps(sendData)
+            s.send(sendData.encode())
+            rcvdData = s.recv(1024).decode()
+
+    elif qtypefirst == 'd':
+        sendData = json.dumps(qtype)
+        s.send(sendData.encode())
+        rcvdData = s.recv(1024).decode()
+        response = json.loads(rcvdData)
+        print(response)
+
+    elif qtypefirst == 'g':
+
+        sendData = json.dumps(qtype)
+        s.send(sendData.encode())
+        rcvdData = s.recv(1024).decode()
+        response = json.loads(rcvdData)
+        if response == '':
+            rcvdData = s.recv(1024).decode()
+            response = json.loads(rcvdData)
+        if response[0:5] == "Error":
+            print(response)
+        elif type(response) == list:
+            print(response[0])
+            print(response[1])
+            for choice in response[2]:
+                print(choice)
+            print(response[3])
+        else:
+            print("Unable to get question")
+
+    elif qtypefirst == 'r':
+
+        sendData = json.dumps(qtype)
+        s.send(sendData.encode())
+        rcvdData = s.recv(1024).decode()
+        response = json.loads(rcvdData)
+
+        if type(response) != list:
+            print("There are no questions entered")
+            qtype = "reset"
+            continue
+        else:
+            number, info = response
+            print(number)
+            print(info[1])
+            for x in info[2]:
+                print(x)
+            useranswer = input("Answer: ")
+
+            sendData = "c " + number + " " + useranswer
+
+            length = len(info[2])
+            charLength = chr(ord('`')+length)
+            stop = 0
+
+            if(useranswer <= charLength and useranswer != ""):
+                sendData = json.dumps(sendData)
+                s.send(sendData.encode())
+                rcvdData = s.recv(1024).decode()
+                response = json.loads(rcvdData)
+                print(response)
+
+            else:
+                while stop == 0:
+                    useranswer = input(
+                        "That's not an option. Please enter your answer: ")
+                    if useranswer == "":
+                        continue
+                    elif(useranswer <= charLength):
+                        sendData = "c " + number + " " + useranswer
+                        sendData = json.dumps(sendData)
+                        s.send(sendData.encode())
+                        rcvdData = s.recv(1024).decode()
+                        response = json.loads(rcvdData)
+                        print(response)
+                        break
+
+    elif qtypefirst == 'c':
+        qtyperest = qtyperest.replace(" ", "")
+        qnumber = qtyperest[:-1]
+        qtrialanswer = qtyperest[-1]
+        qtype = "c " + qnumber + " " + qtrialanswer
+        sendData = json.dumps(qtype)
+        s.send(sendData.encode())
+        rcvdData = s.recv(1024).decode()
+        response = json.loads(rcvdData)
+
+        print(response)
+    elif(qtypefirst == 'k'):
+        print("Ending server")
+        sendData = "End server"
+        sendData = json.dumps(sendData)
+        s.send(sendData.encode())
+
+    elif(qtypefirst == 'q'):
+        print("Ending client")
+        sendData = "Ending client"
+        sendData = json.dumps(sendData)
+        s.send(sendData.encode())
+        break
+
+    elif qtypefirst == 'h':
+        helpinfo = "Options: \n\np: put question in the question bank. Each section is separated with a period on a blank line. Enter tags, question text, choices separated by a period each and then an additional period after all choices have been entered and the correct answer. \n\nd: delete a question from the bank. Enter d followed by the number you wish to delete. \n\nr: get a random question from the bank. It will display the question and list of option. Select the answer you think it is and then hit enter. \n\nc: check answer to a question. Enter c followed by the question number you want to check and then the answer you wish to check in the format c <question number> <answer>. It will return correct or incorrect. \n\nq: terminate the client\n\nk: terminate the server\n\nh: print these instructions"
+        print(helpinfo)
+
+    else:
+        print("Command not recognized")
+
+
+s.close()
---
+++ cserver.py
@@ -0,0 +1,146 @@
+# Allison Kahn edit
+# CNT4007C
+# Project 1
+# Code based on https://stackoverflow.com/questions/21233340/sending-string-via-socket-python
+#!/usr/bin/env python3
+import socket
+import json
+import random
+
+
+def save_obj(obj, name):
+    nameOfFile = "qbank.txt"
+    obj = json.dumps(obj)
+    with open(nameOfFile, 'w') as f:
+        json.dump(obj, f, ensure_ascii=False)
+
+
+def load_obj(name):
+    nameOfFile = "qbank.txt"
+    with open(nameOfFile) as json_file:
+        file = json.load(json_file)
+        return json.loads(file)
+
+
+def firstmakeConnection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', 0))
+    global port
+    port = s.getsockname()[1]
+    print(port)
+    s.listen(5)
+    c, addr = s.accept()
+    return c
+
+
+def makeConnection():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', port))
+    print(port)
+    s.listen(5)
+    c, addr = s.accept()
+    return c
+
+
+c = firstmakeConnection()
+questionDict = {}
+
+try:
+    questionDict = load_obj("questions")
+    if type(questionDict) == str:
+        questionDict = {}
+except (OSError, IOError, EOFError) as e:
+    questionDict = json.dumps({})
+    save_obj(questionDict, "questions")
+    questionDict = {}
+
+
+while True:
+    rcvdData = c.recv(1024).decode()
+    if rcvdData != "":
+        temp = json.loads(rcvdData)
+        tempfirst = ""
+        if type(temp) != dict:
+            tempfirst = temp[:1]
+            temprest = temp[1:]
+
+        if temp == "Nothing to send":
+
+            sendData = ""
+        elif temp == "End server":
+            break
+        elif temp == "Ending client":
+            c.close()
+            c = makeConnection()
+            sendData = ""
+            sendData = json.dumps(sendData)
+        # elif temp == "GetNextQNum":
+        #     for x in range(1, 500):
+        #         x = str(x)
+        #         if x not in questionDict:
+        #             questionNum = x
+        #             break
+        #     sendData = json.dumps(questionNum)
+        elif type(temp) == dict:
+            for key, value in temp.items():
+                questionDict.update({key: value})
+            save_obj(questionDict, "questions")
+            sendData = json.dumps("")
+        elif temp == "GetDict":
+            sendData = json.dumps(questionDict)
+
+        elif tempfirst == "p":
+            temprest = temprest.strip(" ")
+            if temprest in questionDict:
+                sendData = ("Error: question number " +
+                            temprest + " already used.")
+            else:
+                sendData = temprest
+            sendData = json.dumps(sendData)
+
+        elif tempfirst == "d":
+            temprest = temprest.strip(" ")
+            if temprest in questionDict:
+                sendData = ("Deleted question " + temprest)
+                questionDict.pop(temprest)
+            else:
+                sendData = (temprest + " is not available to delete")
+            sendData = json.dumps(sendData)
+        elif tempfirst == "g":
+            temprest = temprest.strip(" ")
+            if temprest in questionDict:
+                sendData = questionDict[temprest]
+            else:
+                sendData = ("Error: question " + temprest + " not found")
+            sendData = json.dumps(sendData)
+        elif tempfirst == "r":
+            if bool(questionDict) == False:
+                sendData = "None"
+            else:
+                sendData = random.choice(list(questionDict.items()))
+            sendData = json.dumps(sendData)
+        elif tempfirst == "c":
+            temprest = temprest[1:]
+            qnumber = temprest.split(" ")[0]
+            answer = temprest.split(" ")[1]
+            if qnumber in questionDict:
+                if questionDict[qnumber][3] == answer:
+                    sendData = "Correct"
+                else:
+                    sendData = "Incorrect"
+            else:
+                sendData = "Question not entered"
+            sendData = json.dumps(sendData)
+        else:
+            # print("It's a dict")
+            print("")
+    else:
+        sendData = json.dumps("")
+
+    c.send(sendData.encode())
+    save_obj(questionDict, "questions")
+
+
+c.close()
